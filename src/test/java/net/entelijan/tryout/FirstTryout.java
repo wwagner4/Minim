ff667b0e2d3586ff7df64e1f27d66643cd2a7232
wwagner4 <wwagner4@gmail.com>
2016-05-15 15:02:49 +0200

    creation of minim works fine

--- src/test/java/net/entelijan/tryout/FirstTryout.java
+++ src/test/java/net/entelijan/tryout/FirstTryout.java
@@ -2,6 +2,7 @@ package net.entelijan.tryout;
 
 import java.io.InputStream;
 
+import ddf.minim.Minim;
 import ddf.minim.javasound.JSMinim;
 import ddf.minim.spi.MinimServiceProvider;
 
@@ -10,9 +11,11 @@ public class FirstTryout
 
 	public static void main(String[] args)
 	{
-		Object fileLoader = new FileLoader();
+		FileLoader fileLoader = new FileLoader();
 		MinimServiceProvider serviceProvider = new JSMinim( fileLoader );
 		System.out.println( "Created a service provider: " + serviceProvider );
+		Minim minim  = new Minim( serviceProvider );
+		System.out.println( "Created minim: " + minim );
 	}
 
 	public static class FileLoader
